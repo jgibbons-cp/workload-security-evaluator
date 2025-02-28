7254703a33799c17cbb4f3fe40b8fe627ab24b70
Jenks Gibbons <jenksgibbons@gmail.com>
2025-02-28 15:23:53 -0700

    make sure system probe is running

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -17,5 +17,16 @@ helm repo add datadog https://helm.datadoghq.com
 helm install dd-operator datadog/datadog-operator -n datadog
 kubectl apply -f dd-agent.yaml -n datadog
 
+# let it start up
+pod_name=$(kubectl get po -n datadog  | grep datadog-agent | awk '{print $1;}')
+
+# make sure system-probe is running
+ret_val=1
+while [ "$ret_val" -ne "0" ]
+do
+  kubectl exec -n datadog $pod_name -- grep "tracing started" /var/log/datadog/system-probe.log
+  ret_val=$?
+done
+
 # watch pods come up
 kubectl get po -n datadog -w
