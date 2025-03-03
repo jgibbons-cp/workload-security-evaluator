60bf25439847ac6de647e5f7ba026247d27851cd
Jenks Gibbons <jenksgibbons@gmail.com>
2025-03-03 12:00:34 -0700

    better error checking to ensure all containers are ready

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -27,20 +27,46 @@ done
 # create agent
 kubectl apply -f dd-agent.yaml -n datadog
 
-# let it start up and grab name
-sleep 10
+# let it start up
+ret_val=1
+while [ "$ret_val" -ne "0" ]
+do
+  kubectl get po -n datadog | grep datadog-agent | grep Running
+  ret_val=$?
+done
+
+# grab name
 pod_name=$(kubectl get po -n datadog  | grep datadog-agent | awk '{print $1;}')
 
 # make sure system-probe is running
 ret_val=1
 while [ "$ret_val" -ne "0" ]
 do
-  kubectl exec -n datadog $pod_name -- grep "tracing started" /var/log/datadog/system-probe.log
+  kubectl get po -n datadog | grep datadog-agent | grep Crash
+  ret_val=$?
+  if [ "$ret_val" -ne "1" ]; then
+    echo "Exiting... agent in CrashLoopBackoff...\n"
+  fi
+
+  kubectl exec -n datadog $pod_name -- grep "tracing started" /var/log/datadog/system-probe.log 2>/dev/null
   ret_val=$?
 done
 
-# give it some more time to get ready for connections to system-probe
-sleep 60
+# make sure cluster agent is running
+ret_val=1
+while [ "$ret_val" -ne "1" ]
+do
+  kubectl get po -n datadog | grep cluster-agent | grep Running
+  ret_val=$?
+done
 
-# watch pods come up
-kubectl get po -n datadog -w
+# ensure all containers are ready
+ret_val=0
+while [ "$ret_val" -ne "1" ]
+do
+  kubectl get po -n datadog | grep -E '(0/1|1/4|2/4|3/4)'
+  ret_val=$?
+done
+
+# all pods are up
+kubectl get po -n datadog
